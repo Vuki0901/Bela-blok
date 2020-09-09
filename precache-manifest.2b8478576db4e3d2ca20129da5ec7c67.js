3910ec249862afe7ed0edab473a0c24e88642b3c
Vuki0901 <leo.vukoje02@gmail.com>
2020-09-09 21:44:48 +0200

    Updates

---
+++ precache-manifest.2b8478576db4e3d2ca20129da5ec7c67.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "572fdcd369d8eb98137abab3517c7b28",
+    "url": "/Bela-blok/index.html"
+  },
+  {
+    "revision": "6298dbaba402932cb8dc",
+    "url": "/Bela-blok/static/css/2.c04d6913.chunk.css"
+  },
+  {
+    "revision": "65d9054d6b0acbe7098e",
+    "url": "/Bela-blok/static/css/main.db235768.chunk.css"
+  },
+  {
+    "revision": "6298dbaba402932cb8dc",
+    "url": "/Bela-blok/static/js/2.faa182db.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Bela-blok/static/js/2.faa182db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65d9054d6b0acbe7098e",
+    "url": "/Bela-blok/static/js/main.79977a6a.chunk.js"
+  },
+  {
+    "revision": "3f9d7169b9340137a6c3",
+    "url": "/Bela-blok/static/js/runtime-main.6a542cf1.js"
+  },
+  {
+    "revision": "ec79c98b018b74415672f25203686ac6",
+    "url": "/Bela-blok/static/media/logo.ec79c98b.png"
+  }
+]);
